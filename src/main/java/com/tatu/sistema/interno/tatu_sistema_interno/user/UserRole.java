aa1900238fe26aad1299d78678969f18ddc9c76e
caio <caio@guiafacil.com>
2025-07-31 15:00:26 -0300

    criando UserRole enum para servir como tipagem de role

---
+++ src/main/java/com/tatu/sistema/interno/tatu_sistema_interno/user/UserRole.java
@@ -0,0 +1,17 @@
+package com.tatu.sistema.interno.tatu_sistema_interno.user;
+
+public enum UserRole {
+
+    EMPLOYEE("employee"),
+    COSTUMER("costumer");
+
+    private String role;
+
+    UserRole(String role){
+        this.role = role;
+    }
+
+    public String getRole(){
+        return role;
+    }
+}
