4f5e4bac8b37bf0732a777f87f00ea1680dda650
caio <caio@guiafacil.com>
2025-07-31 15:54:23 -0300

    criado o service authorizationservice

---
+++ src/main/java/com/tatu/sistema/interno/tatu_sistema_interno/infra/AuthorizationService.java
@@ -0,0 +1,21 @@
+package com.tatu.sistema.interno.tatu_sistema_interno.infra;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.tatu.sistema.interno.tatu_sistema_interno.user.UsersRepository;
+
+@Service
+public class AuthorizationService implements UserDetailsService{
+
+    @Autowired
+    UsersRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByLogin(username);
+    }
+}
