9f75a4ff09846bb9499370766a20fe576ea4fd14
caio <caio@guiafacil.com>
2025-07-31 10:09:14 -0300

    first commit

---
+++ src/main/java/com/tatu/sistema/interno/tatu_sistema_interno/TatuSistemaInternoApplication.java
@@ -0,0 +1,13 @@
+package com.tatu.sistema.interno.tatu_sistema_interno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TatuSistemaInternoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TatuSistemaInternoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tatu/sistema/interno/tatu_sistema_interno/TatuSistemaInternoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tatu.sistema.interno.tatu_sistema_interno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TatuSistemaInternoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
