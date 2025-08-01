5da72839e0df4b068b24f10264a187eb13a45617
caio <caio@guiafacil.com>
2025-08-01 10:51:57 -0300

    arrumando

--- src/main/java/com/tatu/sistema/interno/tatu_sistema_interno/user/UsersRepository.java
+++ src/main/java/com/tatu/sistema/interno/tatu_sistema_interno/user/UsersRepository.java
@@ -3,6 +3,6 @@ package com.tatu.sistema.interno.tatu_sistema_interno.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 
-public interface UsersRepository extends JpaRepository<Users, String>{
+public interface UsersRepository extends JpaRepository<Users, Long>{
     UserDetails findByLogin(String login);
 }
