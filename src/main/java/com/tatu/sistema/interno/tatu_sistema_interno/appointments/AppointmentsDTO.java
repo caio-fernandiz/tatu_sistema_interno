91e4ab282876f31136aabf130a261c5a535ad0a4
caio <caio@guiafacil.com>
2025-08-04 11:11:28 -0300

    também uma correção

--- src/main/java/com/tatu/sistema/interno/tatu_sistema_interno/appointments/AppointmentsDTO.java
+++ src/main/java/com/tatu/sistema/interno/tatu_sistema_interno/appointments/AppointmentsDTO.java
@@ -2,11 +2,11 @@ package com.tatu.sistema.interno.tatu_sistema_interno.appointments;
 
 import java.time.LocalDateTime;
 
-import com.tatu.sistema.interno.tatu_sistema_interno.user.CostumersUsersDTO;
+import com.tatu.sistema.interno.tatu_sistema_interno.user.CustomersUsersDTO;
 
-public record AppointmentsDTO(Long id, LocalDateTime appointmentsDate, CostumersUsersDTO users) {
+public record AppointmentsDTO(Long id, LocalDateTime appointmentsDate, CustomersUsersDTO users) {
     public AppointmentsDTO(Appointments appointments){
-        this(appointments.getId(), appointments.getAppointmentsDate(), new CostumersUsersDTO(appointments.getUser()));
+        this(appointments.getId(), appointments.getAppointmentsDate(), new CustomersUsersDTO(appointments.getUser()));
         
     }
 }
