9c21decb8285ef4c537765878e53be595767672d
caio <caio@guiafacil.com>
2025-08-06 17:59:48 -0300

    atualização em classes para users

--- src/main/java/com/tatu/sistema/interno/tatu_sistema_interno/user/CustomersUsersDTO.java
+++ src/main/java/com/tatu/sistema/interno/tatu_sistema_interno/user/CustomersUsersDTO.java
@@ -1,7 +1,7 @@
 package com.tatu.sistema.interno.tatu_sistema_interno.user;
 
-public record CustomersUsersDTO(String id, String login, String phone, Boolean loyalCostumer) {
+public record CustomersUsersDTO( String login, String phone, Boolean loyalCostumer) {
     public CustomersUsersDTO(Users users){
-        this(users.getId(), users.getLogin(), users.getPhone(), users.getLoyalCustomer());
+        this(users.getLogin(), users.getPhone(), users.getLoyalCustomer());
     }
 }
--- src/main/java/com/tatu/sistema/interno/tatu_sistema_interno/user/CustomersUsersService.java
+++ src/main/java/com/tatu/sistema/interno/tatu_sistema_interno/user/CustomersUsersService.java
@@ -3,12 +3,14 @@ package com.tatu.sistema.interno.tatu_sistema_interno.user;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
 @Service
 public class CustomersUsersService {
 
+    @Autowired
     private final UsersRepository usersRepository;
 
     public CustomersUsersService(UsersRepository usersRepository){
@@ -19,14 +21,13 @@ public class CustomersUsersService {
         
         Optional <Users> user = usersRepository.findByPhone(phone);
 
-        return user.filter(users -> users.getRole() == UserRole.CUSTOMER).map(users -> new CustomersUsersDTO(users.getId(), users.getLogin(), users.getPhone(), users.getLoyalCustomer())).orElseThrow(() -> new RuntimeException("Não encontrado / Não é um cliente"));
-}
-
+        return user.filter(users -> users.getRole() == UserRole.CUSTOMER).map(users -> new CustomersUsersDTO( users.getLogin(), users.getPhone(), users.getLoyalCustomer())).orElseThrow(() -> new RuntimeException("Não encontrado / Não é um cliente"));
+    }
 
     public List<CustomersUsersDTO> findListByRoleCustomer (){
         List <Users> customersUsers = usersRepository.findListByRole(UserRole.CUSTOMER);
 
-        return customersUsers.stream().map(users -> new CustomersUsersDTO(users.getId(), users.getLogin(), users.getPhone(), users.getLoyalCustomer())).toList();
+        return customersUsers.stream().map(CustomersUsersDTO::new).toList();
     }
 
     public CustomersUsersDTO updateStatusCostumer(String phone, Boolean loyalCustomer){
