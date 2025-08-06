33918afeadb02582a702a31276c095ec65b5e3b2
caio <caio@guiafacil.com>
2025-08-06 18:03:28 -0300

    alterações e criação do repository de agendamentos

--- src/main/java/com/tatu/sistema/interno/tatu_sistema_interno/appointments/AppointmentsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tatu.sistema.interno.tatu_sistema_interno.appointments;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AppointmentsRepository extends JpaRepository <Appointments, Long>{
-    
-}
---
+++ src/main/java/com/tatu/sistema/interno/tatu_sistema_interno/scheduled/ScheduledRepository.java
@@ -0,0 +1,12 @@
+package com.tatu.sistema.interno.tatu_sistema_interno.scheduled;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+
+public interface ScheduledRepository extends JpaRepository <Scheduled, Long>{
+    @Query("SELECT a FROM Scheduled a JOIN FETCH a.users")
+    List<Scheduled> findAllWithCustomer();
+}
